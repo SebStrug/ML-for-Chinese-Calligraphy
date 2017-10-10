605aafb6e3b4e347510ea282452442e47bb95e30
SebStrug <SebStrug@users.noreply.github.com>
2017-10-10 16:45:51 +0100

    Tesseract OCR testing

---
+++ TesseractOCRTest.py
@@ -0,0 +1,28 @@
+"""
+Created on Tue Sep 26 13:48:26 2017
+Testing Tesseract-OCR
+@author: Sebastian, Elliot
+"""
+try:
+    import Image
+except ImportError:
+    from PIL import Image
+import pytesseract #python wrapper for Tesseract-OCR
+import mtranslate as mt #translater for Chinese -> English
+
+pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract' #change Tesseract-OCR path location
+tessdata_dir_config = '--tessdata-dir "C:\Program Files (x86)\Tesseract-OCR"' #path for Tesseract-OCR language data
+
+#print(pytesseract.image_to_string(Image.open('test-eng.png'), lang='eng',config=tessdata_dir_config))
+#print(pytesseract.image_to_string(Image.open('test-fonts.png'), lang='eng',config=tessdata_dir_config))
+#print(pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra+ita+spa+por', config=tessdata_dir_config))
+#
+chiCaritas = pytesseract.image_to_string(Image.open('test-chinese.png'), lang='chi_sim+chi_tra', config=tessdata_dir_config)
+print(chiCaritas)
+engTranslation = mt.translate(chiCaritas.encode('utf-8'),"en","auto")
+print(engTranslation)
+
+chiCaritas = pytesseract.image_to_string(Image.open('test-chinese3.gif'), lang='chi_sim+chi_tra', config=tessdata_dir_config)
+print(chiCaritas)
+engTranslation = mt.translate(chiCaritas.encode('utf-8'),"en","auto")
+print(engTranslation)
