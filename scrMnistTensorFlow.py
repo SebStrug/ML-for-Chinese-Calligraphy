7b82cf7de1922332275236f1e79366b9e7b617e8
elliotk123 <32294584+elliotk123@users.noreply.github.com>
2017-10-24 17:06:54 +0100

    now have working cnn mnist

--- scrMnistTensorFlow.py
+++ scrMnistTensorFlow.py
@@ -5,62 +5,46 @@ Created on Tue Oct 24 14:26:02 2017
 @author: ellio
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import argparse
-import sys
-
 from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 import tensorflow as tf
+sess = tf.InteractiveSession()
 
-FLAGS = None
+#create place holders for nodes(inputs and labels)
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+#create variebles for the wieghts and biases
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+#initialise all variables in the seesion
+sess.run(tf.global_variables_initializer())
+
+#calculate the outputs
+y = tf.matmul(x,W) + b
+#%%TRAINING
+
+#caluclate the average cross entropy across a batch between the predictions y_ and the labels y.
+#This is the value to reduce
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+# define the training method to update the wieghts 
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+# put through all training data and update the weights for each batch
+for _ in range(1000):
+  batch = mnist.train.next_batch(100)
+  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+  
+#%%TESTING MODEL
+  
+#caluclate whether the prediction for each image is correct
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+#caluclate the average of all the predictions to get a factional accuracy
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+#print the accuracy
+print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
 
-def main(_):
-  # Import data
-  mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
-  # Create the model
-  x = tf.placeholder(tf.float32, [None, 784])
-  W = tf.Variable(tf.zeros([784, 10]))
-  b = tf.Variable(tf.zeros([10]))
-  y = tf.matmul(x, W) + b
 
-  # Define loss and optimizer
-  y_ = tf.placeholder(tf.float32, [None, 10])
 
-  # The raw formulation of cross-entropy,
-  #
-  #   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
-  #                                 reduction_indices=[1]))
-  #
-  # can be numerically unstable.
-  #
-  # So here we use tf.nn.softmax_cross_entropy_with_logits on the raw
-  # outputs of 'y', and then average across the batch.
-  cross_entropy = tf.reduce_mean(
-      tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-  train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-  sess = tf.InteractiveSession()
-  tf.global_variables_initializer().run()
-  # Train
-  for _ in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-
-  # Test trained model
-  correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  print(sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                      y_: mnist.test.labels}))
-
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
-                      help='Directory for storing input data')
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
