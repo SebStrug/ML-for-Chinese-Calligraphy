74e3be5585158048b5350367cc8ba9ee60d0a823
SebStrug <strugsebastian@gmail.com>
2017-10-24 09:39:19 +0100

    Fixed extend array function
    Small changes to ensure it works in the case where width<maxWidth but height>maxHeight or vice versa.

--- classImageFunctions.py
+++ classImageFunctions.py
@@ -14,16 +14,19 @@ class imageFunc(object):
     def extendArray(array, width, height, maxWidth, maxHeight):
         """Generates a new array of zeros with a size defined by the max height and max width,
         with the original array in question in the centre of that array."""
-        if width < maxWidth or height < maxHeight:
+        if width <= maxWidth and height <= maxHeight:
             newArray = np.zeros((maxHeight,maxWidth))
             lowerBound = maxHeight//2 - height//2
             upperBound = lowerBound+height
             leftBound = maxWidth//2 - width//2
             rightBound = leftBound + width
             newArray[lowerBound:upperBound, leftBound:rightBound] = array
+            return newArray
         else:
-            newArray = array
-        return newArray
+            print("Error, max dimension(s) less than dimension(s) of array. \
+                  Width = {}, Height = {}, Max width = {}, Max Height = {}". \
+                  format(width,height,maxWidth,maxHeight))
+            return array
     
     def scaleImage(path,output):
         """Scales the image by a random value between 0.9 and 1.1"""
--- test extendArray.py
+++ test extendArray.py
@@ -6,12 +6,12 @@ Created on Thu Oct 19 17:39:32 2017
 """
 import numpy as np
 boo = np.ones((4,3))
-maxWidth = 5
-maxHeight=5
+maxWidth = 4
+maxHeight = 3
 def extendArray(array, width, height, maxWidth, maxHeight):
     """Generates a new array of zeros with a size defined by the max height and max width,
     with the original array in question in the centre of that array."""
-    if width < maxWidth or height < maxHeight:
+    if width <= maxWidth and height <= maxHeight:
         newArray = np.zeros((maxHeight,maxWidth))
         lowerBound = maxHeight//2 - height//2
         upperBound = lowerBound+height
@@ -20,6 +20,9 @@ def extendArray(array, width, height, maxWidth, maxHeight):
         newArray[lowerBound:upperBound, leftBound:rightBound] = array
         return newArray
     else:
+        print("Error, max dimension(s) less than dimension(s) of array. \
+              Width = {}, Height = {}, Max width = {}, Max Height = {}". \
+              format(width,height,maxWidth,maxHeight))
         return array
 print(boo)
 print(extendArray(boo,boo.shape[1],boo.shape[0],maxWidth,maxHeight))
