e199d8c40320af6ade7d6b0f575f1f7254c08431
SebStrug <strugsebastian@gmail.com>
2017-10-26 15:12:27 +0100

    Saved 10 gnt files

--- dataHandling/scrMultipleGNTtoNPZ.py
+++ dataHandling/scrMultipleGNTtoNPZ.py
@@ -11,7 +11,7 @@ import numpy as np
 
 #file Path for functions
 #funcPath = 'C:/Users/ellio/OneDrive/Documents/GitHub/ML-for-Chinese-Calligraphy/'
-funcPath = 'C:\\Users\\Sebastian\\Desktop\\GitHub\\ML-for-Chinese-Calligraphy'
+funcPath = 'C:\\Users\\Sebastian\\Desktop\\GitHub\\ML-for-Chinese-Calligraphy\\dataHandling'
 os.chdir(funcPath)
 from classFileFunctions import fileFunc as fF
 
@@ -27,16 +27,16 @@ dataInfo = fF.infoGNT(data,tot)
 dataForSaving = fF.arraysFromGNT(data,dataInfo)
 
 data=0;#delete data in raw byte form 
-fF.saveNPZ(dataPath,"1001to1004",saveLabels=dataForSaving[0],saveImages=dataForSaving[5])
+fF.saveNPZ(dataPath,"1001to1010-c",saveLabels=dataForSaving[0],saveImages=dataForSaving[5])
 
 """Look at the characters in the data"""
-characters = dataForSaving[0]
-numChars = len(list(set(characters)))
-print(list(set(np.sort(characters))))
-#try out the 1hot vector method
-characters = [ord(i) for i in characters]
-oneHots = np.zeros((len(characters), max(characters)+1)) #array of zeros 
-oneHots[np.arange(len(characters)), characters] = 1 #replace a 0 in each vector with a 1 according to the labels
+#characters = dataForSaving[0]
+##numChars = len(list(set(characters)))
+##print(list(set(np.sort(characters))))
+##try out the 1hot vector method
+#characters = [ord(i) for i in characters]
+#oneHots = np.zeros((len(characters), max(characters)+1)) #array of zeros 
+#oneHots[np.arange(len(characters)), characters] = 1 #replace a 0 in each vector with a 1 according to the labels
 
 
 
