f4e1aa15c9fe6a47df52def8bc839b573ebf8b04
elliotk123 <32294584+elliotk123@users.noreply.github.com>
2017-10-31 17:03:11 +0000

    added my path

--- dataHandling/scrMultipleGNTtoNPZ.py
+++ dataHandling/scrMultipleGNTtoNPZ.py
@@ -12,7 +12,7 @@ import numpy as np
 #file Path for functions
 funcPathElliot = 'C:/Users/ellio/OneDrive/Documents/GitHub/ML-for-Chinese-Calligraphy/'
 funcPathSeb = 'C:\\Users\\Sebastian\\Desktop\\GitHub\\ML-for-Chinese-Calligraphy\\dataHandling'
-os.chdir(funcPathSeb)
+os.chdir(funcPathElliot)
 from classFileFunctions import fileFunc as fF
 from classMachineLearning import machineLearning as ML
 
@@ -32,12 +32,13 @@ dataForSaving = fF.arraysFromGNT(data,dataInfo)
 data=0;#delete data in raw byte form 
 
 savePathSeb = 'C:\\Users\\Sebastian\\Desktop\\MLChinese\\Saved script files'
-fF.saveNPZ(savePathSeb,"{}Files-characters-images".format(numFiles),saveLabels=dataForSaving[0],\
+savePathElliot='C:\\Users\\ellio\\Documents\\training data'
+fF.saveNPZ(savePathElliot,"{}Files-characters-images".format(numFiles),saveLabels=dataForSaving[0],\
            saveImages=dataForSaving[5])
 #fF.saveNPZ(savePathSeb,"{}Files-hotOnes-images.format(numFiles)",\
 #           saveLabels=ML.newHotOnes(dataForSaving[0],'zippedListTest',savePathSeb),\
 #           saveImages=dataForSaving[5])
-fF.saveNPZ(savePathSeb,"{}Files-charNums-images".format(numFiles),\
+fF.saveNPZ(savePathElliot,"{}Files-charNums-images".format(numFiles),\
            saveLabels=[ML.storeCharNumber(i,'zippedListTest',savePathSeb) for i in dataForSaving[0]],\
            saveImages=dataForSaving[5])
 
