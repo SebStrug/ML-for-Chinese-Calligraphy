e0f63f6cc6911eadf427721bed0111be211ae716
elliotk123 <32294584+elliotk123@users.noreply.github.com>
2017-11-16 12:33:32 +0000

    getting simple net working with t board

--- tensorFlow/scrSimpleNetworkTensorBoard.py
+++ tensorFlow/scrSimpleNetworkTensorBoard.py
@@ -24,7 +24,7 @@ import datetime
 user = "Elliot"
 #user = "Seb"
 
-funcPathElliot = 'C:/Users/ellio/OneDrive/Documents/GitHub/ML-for-Chinese-Calligraphy/dataHandling'
+funcPathElliot = 'C:/Users/ellio/OneDrive/Documents/GitHubPC/ML-for-Chinese-Calligraphy/dataHandling'
 funcPathSeb = 'C:\\Users\\Sebastian\\Desktop\\GitHub\\ML-for-Chinese-Calligraphy\\dataHandling'
 dataPathElliot = 'C:/Users/ellio/Documents/training data/Machine Learning data/'
 dataPathSeb = 'C:\\Users\\Sebastian\\Desktop\\MLChinese\\CASIA\\Converted\\All C Files'
@@ -63,7 +63,7 @@ def oneHot(numberList,n):
         oneHotArray[j][numberList[j]] = 1;
     return oneHotArray;
 
-trainRatio = 0.95
+trainRatio = 0.90
 
 
 print("splitting data...")
@@ -112,35 +112,19 @@ def fc_layer(input, size_in, size_out, name="fc"):
 numOutputs = 3755
 inputDim = 40
 
-def mnist_model(learning_rate, use_two_conv, use_two_fc, hparam):
+def mnist_model(learning_rate, hparam):
   tf.reset_default_graph()
   sess = tf.InteractiveSession()
   with sess.as_default():
       # Setup placeholders, and reshape the data
-      x = tf.placeholder(tf.float32, shape=[None, inputDim^2], name="x")
+      x = tf.placeholder(tf.float32, shape=[None, pow(inputDim,2)], name="x")
       x_image = tf.reshape(x, [-1, inputDim, inputDim, 1])
       tf.summary.image('input', x_image, 3)
-      y = tf.placeholder(tf.float32, shape=[None, 3755], name="labels")
+      y = tf.placeholder(tf.float32, shape=[None, numOutputs], name="labels")
     
-      if use_two_conv:
-        conv1 = conv_layer(x_image, 1, 32, "conv1")
-        conv_out = conv_layer(conv1, 32, 64, "conv2")
-      else:
-        conv1 = conv_layer(x_image, 1, 64, "conv")
-        conv_out = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
-    
-      flattened = tf.reshape(conv_out, [-1, 10 * 10 * 64])
-    
-    
-      if use_two_fc:
-        fc1 = fc_layer(flattened, 7 * 7 * 64, 1024, "fc1")
-        embedding_input = fc1
-        embedding_size = 1024
-        logits = fc_layer(fc1, 1024, 10, "fc2")
-      else:
-        embedding_input = flattened
-        embedding_size = 10*10*64
-        logits = fc_layer(flattened, 10*10*64, 3755, "fc")
+      embedding_input = x
+      embedding_size = pow(inputDim,2)
+      logits = fc_layer(x, pow(inputDim,2), numOutputs, "fc")
     
       with tf.name_scope("xent"):
         xent = tf.reduce_mean(
@@ -177,10 +161,10 @@ def mnist_model(learning_rate, use_two_conv, use_two_fc, hparam):
     #  # Specify the width and height of a single thumbnail.
     #  embedding_config.sprite.single_image_dim.extend([28, 28])
     #  tf.contrib.tensorboard.plugins.projector.visualize_embeddings(writer, config)
-      batchSize = 200
-      iterations = 32000
-      displayNum = 200
-      testNum = 600
+      batchSize = 800
+      iterations = 64000
+      displayNum = 800
+      testNum = 3200
       i=0
       print("took ",t.time()-startTime," seconds\n")
       while i<iterations:
@@ -203,24 +187,21 @@ def mnist_model(learning_rate, use_two_conv, use_two_fc, hparam):
           #train_step.run(feed_dict={x: batchImages, y: batchLabels, keep_prob: 0.5})
           i+=batchSize
 
-def make_hparam_string(learning_rate, use_two_fc, use_two_conv):
-  conv_param = "conv=2" if use_two_conv else "conv=1"
-  fc_param = "fc=2" if use_two_fc else "fc=1"
-  return "lr_%.0E,%s,%s" % (learning_rate, conv_param, fc_param)
+def make_hparam_string(learning_rate):
+  fc_param = "fc=1"
+  return "lr_%.0E,%s" % (learning_rate, fc_param)
 
 def main():
   # You can try adding some more learning rates
   for learning_rate in [1E-4]:
 
     # Include "False" as a value to try different model architectures
-    for use_two_fc in [False]:
-      for use_two_conv in [False]:
         # Construct a hyperparameter string for each one (example: "lr_1E-3,fc=2,conv=2)
-        hparam = make_hparam_string(learning_rate, use_two_fc, use_two_conv)
-        print('Starting run for %s' % hparam)
+    hparam = make_hparam_string(learning_rate)
+    print('Starting run for %s' % hparam)
 
 	    # Actually run with the new settings
-        mnist_model(learning_rate, use_two_fc, use_two_conv, hparam)
+    mnist_model(learning_rate, hparam)
 
 
 if __name__ == '__main__':
