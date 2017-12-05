1e7406a14a03c637447f73fd998f91c31208f058
elliotk123 <32294584+elliotk123@users.noreply.github.com>
2017-12-05 15:02:13 +0000

    restored dandelions script

--- tensorFlow/scrSimpleNetworkTensorBoard.py
+++ tensorFlow/scrSimpleNetworkTensorBoard.py
@@ -142,10 +142,10 @@ def conv_layer(input, size_in, size_out, name="conv"):
 def fc_layer(input, size_in, size_out, name="fc"):
   with tf.name_scope(name):
     w = tf.Variable(tf.truncated_normal([size_in, size_out], stddev=0.03), name="W")
-    #b = tf.Variable(tf.constant(0.1, shape=[size_out]), name="B")
-    act = tf.nn.relu(tf.matmul(input, w))
+    b = tf.Variable(tf.constant(0.1, shape=[size_out]), name="B")
+    act = tf.nn.relu(tf.matmul(input, w)+b)
     tf.summary.histogram("weights", w)
-    #tf.summary.histogram("biases", b)
+    tf.summary.histogram("biases", b)
     tf.summary.histogram("activations", act)
     return act
 
