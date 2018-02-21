2440774366927028a6b00402645acaafcacf3472
SebStrug <strugsebastian@gmail.com>
2018-02-21 18:09:59 +0000

    Update to hacknight

--- ES_hacknight1.py
+++ ES_hacknight1.py
@@ -5,3 +5,12 @@ Created on Wed Feb 21 18:08:25 2018
 @author: Sebastian
 """
 
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+dataPathSeb = "C:\\Users\\Sebastian\\Desktop\\GitHub\\hacknight_1\\data"
+
+dataPath = dataPathSeb
+os.chdir(dataPath)
