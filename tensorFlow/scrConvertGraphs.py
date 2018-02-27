994a969e4bca168b5007f5a716b079592f92f58e
SebStrug <strugsebastian@gmail.com>
2018-02-27 15:43:30 +0000

    Updated convert graphs

--- tensorFlow/scrConvertGraphs.py
+++ tensorFlow/scrConvertGraphs.py
@@ -7,12 +7,10 @@ Created on Tue Dec 12 11:27:13 2017
 import os
 import csv
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import glob
 import numpy as np
-
-savePathSeb = "C:/Users/Sebastian/Desktop/MLChinese/Saved script files/Saved graphs"
-
-savePath = savePathSeb
+from datetime import datetime
 
 #%% basic functions
 
@@ -35,62 +33,30 @@ def findPlateau(values,limit):
             break
     return values[i]
 
-#%% manipulate data
-
 def convertData(accData):
     """Converts data from strings into usable things, plus wall time"""
-    #Get info from accuracy
-    headers = accData[0]
-    wallTime = [float(i[0]) for i in accData[1:]] 
-    wallTime = [convWallTime(i) for i in wallTime]  
-    stepTime = [int(i[1])/(10**4) for i in accData[1:]]
-    accValues = [float(i[2][0:5]) for i in accData[1:]]
-    
-#    #Get values from xent
-#    xentValues = [float(i[2][0:5]) for i in xentData[1:]]
-#    minXent = min(xentValues); maxXent = max(xentValues)
-#    #normalise the xent values
-#    normXent = [(i - minXent)/(maxXent - minXent) for i in xentValues]
-    return wallTime,stepTime,accValues#,normXent
+    relTime = []; stepTime = []; accValues = [];
+    for i in range(len(accData)):
+        print(i)
+        wallTime_one = [float(j[0]) for j in accData[i][1:]] # get time in seconds
+        #wallTime_one = [j - wallTime_one[0] for j in wallTime_one] #get relative time
+        wallTime_one = [datetime.fromtimestamp(i) for i in wallTime_one]
+        
+        #wallTime_one = [matplotlib.dates.date2num(i) for i in wallTime_one]
+#        relativeTime_one = [convWallTime(j) for j in wallTime_one]  # convert to hours, mins, seconds
+        stepTime_one = [int(j[1])/(10**4) for j in accData[i][1:]] # get the step count
+        accValues_one = [float(j[2][0:5]) for j in accData[i][1:]] #get the accuracy values
+        
+        relTime.append(wallTime_one)
+        stepTime.append(stepTime_one)
+        accValues.append(accValues_one)
+    return relTime, stepTime, accValues
 
-
-#%% plotting functions
-
-def plotSingle(stepTime,value,name):
-    """Plots a single value for step time, e.g. name = "Accuracy" """
-    lines = plt.plot(stepTime,value)
-    plt.ylim(ymin=0,ymax=1)
-    plt.xlim(xmin=0)
-    plt.grid(True)
-    plt.xlabel('Step time')
-    plt.ylabel(name)
-    plt.setp(lines, color='darkorange', linewidth=2.0)
-
-def plotAccXent(stepTime,accValues,normXent):
-    """Plots accuracy and xent along stepTime"""    
-    accPlateau = findPlateau(accValues,0.01) #0.5% limit for plateau
-    stepTimePlateau = stepTime[accValues.index(accPlateau)]
-    plt.annotate('Accuracy: {}'.format(accPlateau), \
-                 xy=(stepTimePlateau, accPlateau), \
-                 xytext=(stepTimePlateau+30,accPlateau-0.3), \
-                 horizontalalignment='right', verticalalignment='top',\
-                 arrowprops=dict(width = 1, facecolor='gray', shrink=0.01))
-    accuracy = plt.plot(stepTime,accValues)
-    xent = plt.plot(stepTime,xentValues)
-    plt.gca().set_color_cycle(['darkorange','slateblue'])
-#    plt.legend([accuracy, xent], ['Accuracy','Cross entropy'])
-#    plt.ylim(ymin=0,ymax=1)
-#    plt.xlim(xmin=0)
-    plt.grid(True)
-    plt.xlabel('Step time')
-    plt.ylabel('Value')
-    plt.show()
-
-#%% load in files
-"""tensorboard csv to graph function"""
-#change path so figure saves correctly
-nameFile = "/fc1_out10_batch1024_LR"
-os.chdir(savePath+nameFile)
+def smooth(y, box_pts):
+    """Smooths with a moving average box (convolution)"""
+    box = np.ones(box_pts)/box_pts
+    y_smooth = np.convolve(y, box, mode='same')
+    return y_smooth
 
 def loadFiles(savePath,nameFile):
     files = glob.glob(savePath+nameFile+"\*.csv")
@@ -101,43 +67,41 @@ def loadFiles(savePath,nameFile):
         allAcc.append(accOneFile)
     return allAcc
 
-accData = loadFiles(savePath,nameFile)
-wallTime = []; stepTime = []; accValues = []
-for i in accData:
-    singleWallTime,singleStepTime,singleAccValues = convertData(i)
-    #delete every second value
-    deleteNum = 10
-    del singleStepTime[::deleteNum]; del singleWallTime[::deleteNum]; del singleAccValues[::deleteNum]
-    wallTime.append(singleWallTime)
-    stepTime.append(singleStepTime)
-    accValues.append(singleAccValues)
-
-batchSize = 1024
-numOutputs = 50
-trainRatio = 0.8
-epochLength = (300*trainRatio*numOutputs)/batchSize
-
-def smooth(y, box_pts):
-    """Smooths with a moving average box (convolution)"""
-    box = np.ones(box_pts)/box_pts
-    y_smooth = np.convolve(y, box, mode='same')
-    return y_smooth
-
-def plotAll(wallTime,stepTime,accValues):
+def plotAll(relTime,stepTime,accValues,plot_step=False):
     plt.figure()
     for i in range(len(accValues)):
-        #plot charts
-#        colours = ['salmon','royal blue','pale green','peach','grey',\
-#                   'orange','dark read','khaki','seafoam','goldenrod']
-        lines = plt.plot(stepTime[i],smooth(accValues[i],3))#, colours[i])
-        plt.ylim(ymin=0,ymax=1)
-        plt.xlim(xmin=0)
+        
+        if plot_step == True: #if we want to plot the step count
+            lines = plt.plot(stepTime[i],smooth(accValues[i],1))
+            plt.ylim(ymin=0,ymax=1)
+            plt.xlim(xmin=0)
+            plt.xlabel('Iterations *10^4')
+        else:
+            plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+            lines = plt.plot(relTime[i],smooth(accValues[i],1))
+            plt.gcf().autofmt_xdate()
+            plt.xlabel('Time in hours:minutes')
+        
+        plt.title('No. of outputs: {}, Batch size: {}'.\
+                      format(numOutputs,batchSize))
         plt.grid(True)
-        plt.title('No. of outputs: {}, Batch size: {}, training ratio: {}, Epoch length: {}'.\
-                  format(numOutputs,batchSize,trainRatio,int(epochLength)))
-        plt.xlabel('Iterations *10^4')
         plt.ylabel('Accuracy')
         plt.setp(lines, linewidth=2.0)
         plt.savefig("Figure.svg")
+        plt.savefig("Figure.png")
 
-plotAll(wallTime,stepTime,accValues)
+#%% load in files
+"""tensorboard csv to graph function"""
+#change path so figure saves correctly
+savePathSeb = "C:/Users/Sebastian/Desktop/MLChinese/Saved_runs/"
+savePath = savePathSeb
+nameFile = "2018-02-23/6_conv_shuffle_true_normalised"
+os.chdir(savePath+nameFile)
+
+accData = loadFiles(savePath,nameFile)
+relTime, stepTime, accValues = convertData(accData)
+
+batchSize = 128
+numOutputs = 10
+
+plotAll(relTime, stepTime, accValues, plot_step=False)
