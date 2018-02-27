6264c4e63e044c56abc6ffa62573b420e9357c9e
SebStrug <strugsebastian@gmail.com>
2018-02-27 16:56:20 +0000

    Work

--- dataHandling/scrConvertCalligraphy.py
+++ dataHandling/scrConvertCalligraphy.py
@@ -31,27 +31,15 @@ for image in thresholdedImages:
         resizingFactor = max(height,width)/48
         if max(height,width) == height:
             image = image.resize((48,int(width/resizingFactor)),Image.ANTIALIAS) #keep aspect ratio
+            old_size = image.size
+            new_im = Image.new("RGB", (48,48))   ## luckily, this is already black!
+            new_im.paste(image, ((48-old_size[0])/2,(48-old_size[1])/2))
         else:
             image = image.resize(int(height/resizingFactor,48),Image.ANTIALIAS) #keep aspect ratio
     else: #if already square resize the images to 48x48 
         image = image.resize((48,48),Image.ANTIALIAS)
     thresholdedImages_resized.append(image)
     
-def scale(image, max_size, method=Image.ANTIALIAS):
-    """
-    resize 'image' to 'max_size' keeping the aspect ratio 
-    and place it in center of white 'max_size' image 
-    """
-    im_aspect = float(image.size[0])/float(image.size[1])
-    out_aspect = float(max_size[0])/float(max_size[1])
-    if im_aspect >= out_aspect:
-        scaled = image.resize((max_size[0], int((float(max_size[0])/im_aspect) + 0.5)), method)
-    else:
-        scaled = image.resize((int((float(max_size[1])*im_aspect) + 0.5), max_size[1]), method)
- 
-    offset = (((max_size[0] - scaled.size[0]) / 2), ((max_size[1] - scaled.size[1]) / 2))
-    back = Image.new("RGB", max_size, "white")
-    back.paste(scaled, offset)
-    return back
+
 
 
