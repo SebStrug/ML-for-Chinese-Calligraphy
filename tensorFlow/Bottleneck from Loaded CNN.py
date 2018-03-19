0ecac47b90c57092cd9d93b179f221764b10c597
elliotk123 <32294584+elliotk123@users.noreply.github.com>
2018-03-19 14:03:47 +0000

    now saves bottlenecks into several parts

--- tensorFlow/Bottleneck from Loaded CNN.py
+++ tensorFlow/Bottleneck from Loaded CNN.py
@@ -19,6 +19,8 @@ inputDim = 48
 inputChars= 3866#number of unique characters in dataset
 numOutputs= 100#number of outputs in original network
 bottleneckLength = 1024
+batchSize = 1024
+batchesPerFile = 320
 #set paths and file names
 dataPath, LOGDIR, rawDatapath = fF.whichUser("Elliot")
 relTrainDataPath = "Machine learning data/TFrecord"#path of training data relative to datapath in classFileFunc
@@ -36,6 +38,7 @@ train_tfrecord_filename = os.path.join(os.path.join(dataPath,relTrainDataPath),'
 test_tfrecord_filename = os.path.join(os.path.join(dataPath,relTrainDataPath),'test'+str(inputChars)+'.tfrecords')
    
  #%% Open a tensorflow session
+start1 = t.time()
 print("Importing graph.....")
 start = t.time()
 tf.reset_default_graph()
@@ -51,9 +54,8 @@ saver.restore(sess,'./'+modelName)
 graph = tf.get_default_graph()
 #print(graph.get_operations())
 train_kwargs = {"normalize_images": True, "augment_images": False, "shuffle_data": True}
-train_image_batch, train_label_batch = inputs('train',train_tfrecord_filename,bottleneckLength,1,**train_kwargs)
-test_image_batch, test_label_batch = inputs('test',test_tfrecord_filename,bottleneckLength,1,**train_kwargs)
-    
+train_image_batch, train_label_batch = inputs('train',train_tfrecord_filename,batchSize,1,**train_kwargs)
+test_image_batch, test_label_batch = inputs('test',test_tfrecord_filename,batchSize,1,**train_kwargs)
 x=graph.get_tensor_by_name("images:0")
 y_=graph.get_tensor_by_name("labels:0")
 keep_prob=graph.get_tensor_by_name("keep_prob:0")
@@ -67,40 +69,79 @@ print("took ",t.time()-start," seconds\n")
 
 #train data
 print("Extracting Bottlenecks for train data......")
-start = t.time()
+start2=t.time()
 trainBottlenecks=np.zeros((0,bottleneckLength))
 trainLabels = np.asarray([])
+
 try:
+    print("Extracting batches.....")
+    start = t.time()
+    i = 1
+    file=1
     while True:
         trainImageBatch,trainLabelBatch=sess.run([train_image_batch,train_label_batch])
         bottleneckBatch=sess.run(getBottleneck,feed_dict={x: trainImageBatch, keep_prob: 1.0})
+        trainImageBatch = 0
         trainBottlenecks=np.concatenate((trainBottlenecks,bottleneckBatch),axis=0)
         trainLabels=np.concatenate((trainLabels,trainLabelBatch))
+        if i%batchesPerFile == 0 or len(trainLabelBatch) != batchSize:
+            print("extraction took ",t.time()-start," seconds\n") 
+            print("Saving Train Bottlenecks.....")
+            start=t.time()
+            fF.saveNPZ(os.path.join(dataPath,relBottleneckSavePath),\
+                       "bottleneck_"+SaveName+"_{}to{}chars_train_pt{}".format(numOutputs,inputChars,int(file)),\
+                       bottlenecks=trainBottlenecks,labels = trainLabels )
+            bottleneckBatch = 0
+            trainLabelBatch = 0
+            trainBottlenecks=np.zeros((0,bottleneckLength))
+            trainLabels = np.asarray([])
+            print("Save took ",t.time()-start," seconds\n") 
+            print("Extracting batches.....")
+            start = t.time()
+            file+=1
+        i+=1
+#        trainBottlenecks=np.concatenate((trainBottlenecks,bottleneckBatch),axis=0)
+#        trainLabels=np.concatenate((trainLabels,trainLabelBatch))
 except tf.errors.OutOfRangeError:
     print("done")
-    print("took ",t.time()-start," seconds\n")
-print("Saving Train Bottlenecks.....")
-start = t.time()
-fF.saveNPZ(os.path.join(dataPath,relBottleneckSavePath),"bottleneck_"+SaveName+"_{}to{}chars_train".format(numOutputs,inputChars),\
-           bottlenecks=trainBottlenecks,labels = trainLabels )
-print("took ",t.time()-start," seconds\n")   
+    print("train data took ",t.time()-start2," seconds\n")
 
 #test data
 print("Extracting Bottlenecks for test data......")
-start = t.time() 
+start2=t.time()
 testBottlenecks=np.zeros((0,bottleneckLength))
-testLabels = np.asarray([]) 
+testLabels = np.asarray([])
 try:
-    while True: 
+    print("Extracting batch.....")
+    start = t.time()
+    i = 1
+    file=1
+    while True:
         testImageBatch,testLabelBatch=sess.run([test_image_batch,test_label_batch])
         bottleneckBatch=sess.run(getBottleneck,feed_dict={x: testImageBatch, keep_prob: 1.0})
+        testImageBatch = 0
         testBottlenecks=np.concatenate((testBottlenecks,bottleneckBatch),axis=0)
-        testLabels=np.concatenate((testLabels,testLabelBatch)) 
+        testLabels=np.concatenate((testLabels,testLabelBatch))
+        if i%batchesPerFile == 0 or len(testLabelBatch) != batchSize:
+            print("extraction took ",t.time()-start," seconds\n") 
+            print("Saving Test Bottlenecks.....")
+            start=t.time()
+            fF.saveNPZ(os.path.join(dataPath,relBottleneckSavePath),\
+                       "bottleneck_"+SaveName+"_{}to{}chars_test_pt{}".format(numOutputs,inputChars,int(file)),\
+                       bottlenecks=testBottlenecks,labels = testLabels )
+            bottleneckBatch = 0
+            testLabelBatch = 0
+            testBottlenecks=np.zeros((0,bottleneckLength))
+            testLabels = np.asarray([])
+            print("Save took ",t.time()-start," seconds\n")   
+            print("Extracting batches.....")
+            start = t.time()
+            file+=1
+        i+=1
+#        trainBottlenecks=np.concatenate((trainBottlenecks,bottleneckBatch),axis=0)
+#        trainLabels=np.concatenate((trainLabels,trainLabelBatch))
 except tf.errors.OutOfRangeError:
     print("done")
-    print("took ",t.time()-start," seconds\n")
-print("Saving Test Bottlenecks.....")
-start = t.time()
-fF.saveNPZ(os.path.join(dataPath,relBottleneckSavePath),"bottleneck_"+SaveName+"_{}to{}chars_test".format(numOutputs,inputChars),\
-           bottlenecks=testBottlenecks,labels = testLabels )
-print("took ",t.time()-start," seconds\n")
+    print("test data took ",t.time()-start2," seconds\n")
+
+print("Whole process took ",t.time()-start1," seconds")
