e99c194936acabaf2a55e1431f2858fb4ffd3361
SebStrug <SebStrug@users.asnoreply.github.com>
2018-04-24 16:02:34 +0100

    updated neural net to visualise filters while running

--- tensorFlow/netAsFunctions_TFRecord.py
+++ tensorFlow/netAsFunctions_TFRecord.py
@@ -6,11 +6,15 @@ Look at the bottom of the file for all the inputs
 
 import tensorflow as tf
 from tryBuildNet import buildNet
+import itertools
 import time
 import math #to use radians in rotating the image
+import numpy as np
+import matplotlib.pyplot as plt
 import random
 from classDataManip import makeDir
 import os
+import scipy as sp
 from InputTFRecord import inputs
 gitHubRep = os.path.normpath(os.getcwd() + os.sep + os.pardir)# find github path
 #import own functions and classes
@@ -21,7 +25,53 @@ from classFileFunctions import fileFunc as fF
 # phase of execution, e.g. training vs. validation.
 # Initialize `iterator` with training data.
 
+#%%Func defs for visualising filters
+def removeAndSwapAxes(array):
+    foo=np.swapaxes(array,2,3)
+    boo=np.swapaxes(foo,1,2)
+    return boo
+    
+def show_activations(features,featureDim, num_out, path, name, show = False, save = False):
+    test_images = features
 
+    size_figure_grid = int(num_out/8)
+    fig, ax = plt.subplots(8, size_figure_grid, figsize=(8, int(8*8/size_figure_grid)))
+    print("Number of images: {}, size of grid: {}".format(num_out, size_figure_grid))
+    for i, j in itertools.product(range(8), range(size_figure_grid)):
+        ax[i, j].get_xaxis().set_visible(False)
+        ax[i, j].get_yaxis().set_visible(False)
+
+    for k in range(num_out):
+        i = k // size_figure_grid
+        j = k % size_figure_grid
+        ax[i, j].cla()
+        ax[i, j].imshow(np.reshape(test_images[k], (featureDim, featureDim)) )#, cmap='gray')
+    #label = 'Epoch {0}'.format(num_epoch)
+    #fig.text(0.5, 0.04, label, ha='center')
+    if save:
+        os.chdir(path)
+        plt.savefig(name)
+    if show:
+        plt.show()
+    else:
+        plt.close() 
+        
+def maximum_activation(layerActivations):
+    layerHighest = []
+    activationTot = np.sum(layerActivations,axis=(2,3))
+    maxIndices = np.argmax(activationTot,axis=0)
+    for i in range(0,layerActivations.shape[1]):
+            layerHighest.append(layerActivations[int(maxIndices[i])][i])
+    return layerHighest
+        
+def convert_weight_filters(layerWeights):
+    weight_filters = [layerWeights[:,:,:,i] for i in range(layerWeights.shape[3])]
+    weight_filters = [np.sum(i,axis=2) for i in weight_filters]
+    weight_upscaled = [sp.misc.imresize(weight_filters[i],10.0) for i \
+                        in range(layerWeights.shape[3])]
+    return weight_upscaled
+
+#%%
 def run_training():
     tf.reset_default_graph()
         
@@ -96,10 +146,14 @@ def run_training():
     
     #this is the operation that initialises all the graph variables
     init_op = tf.group(tf.global_variables_initializer(),\
-                       tf.local_variables_initializer())        
+                       tf.local_variables_initializer())      
     
+    print("Starting session...")
     with tf.Session() as sess:
-                
+        graph = tf.get_default_graph()
+        conv1Activations = graph.get_tensor_by_name("conv_1/Relu:0")
+        conv1Weights = graph.get_tensor_by_name("conv_1/Variable:0")
+        
 #        tf.global_variables_initializer().run()    
             
         # Create writers
@@ -127,26 +181,46 @@ def run_training():
 #            tf.image_summary('conv_1/weights', weights_transposed, max_images=10)
         
         try:
+            print("Starting training...")
             step = 0
             while True: #train until we run out of epochs
                 train_images, train_labels = sess.run([train_image_batch,train_label_batch])
                 test_images, test_labels = sess.run([test_image_batch,test_label_batch])      
-            
+                
+                if step == 0:
+                    image_for_visualisation = [train_images[0]]
+                
                 if step % 30 == 0:
                     train_accuracy, train_summary = sess.run([accuracy, mergedSummaryOp], \
                                  feed_dict={x: train_images, \
-                                            y_: tf.one_hot(train_labels,num_output).eval()})#,\
-                                            #keep_prob: 1.0})
+                                            y_: tf.one_hot(train_labels,num_output).eval(),\
+                                            keep_prob: 1.0})
                     train_writer.add_summary(train_summary, step)
                     print('Step: {}, Training accuracy = {:.3}'.format(step, train_accuracy))
+                    
+                    # Visualise the activations for this one image
+                    layer1Activations=sess.run(conv1Activations,feed_dict={x: image_for_visualisation, keep_prob: 1.0})
+                    layer1Activations=removeAndSwapAxes(layer1Activations)
+                    layerHighest = maximum_activation(layer1Activations)
+                    show_activations(layerHighest, len(layerHighest[0]), len(layerHighest), \
+                                     "C:\\Users\\Sebastian\\Desktop\\MLChinese\\Visualising_filters\\100Out_Visualise_whileTraining",\
+                                     "layer1Features_step{}.jpg".format(step), show=False, save=True)
+                    # Visualise the weights
+                    layer1Weights=sess.run(conv1Weights)
+                    weights_upscaled = convert_weight_filters(layer1Weights)
+                    show_activations(weights_upscaled, len(weights_upscaled[0]), len(weights_upscaled),\
+                                     "C:\\Users\\Sebastian\\Desktop\\MLChinese\\Visualising_filters\\100Out_Visualise_whileTraining", \
+                                     'layer1Weights_step{}.jpg'.format(step),\
+                                     show=False, save=True)
                     
                 if step % 90 == 0:
                     print("Testing the net...")
                     test_accuracy, test_summary = sess.run([accuracy,mergedSummaryOp], \
                                    feed_dict={x: test_images,\
-                                              y_: tf.one_hot(test_labels,num_output).eval()})#,\
-                                              #keep_prob: 1.0})
+                                              y_: tf.one_hot(test_labels,num_output).eval(),\
+                                              keep_prob: 1.0})
                     test_writer.add_summary(test_summary, step)
+                    
                     if test_accuracy > maxAccuracy:
                         maxAccuracy=test_accuracy
                         saver.save(sess, os.path.join(LOGDIR, "LR{}_Iter{}_TestAcc{:.3}.ckpt".\
@@ -155,8 +229,8 @@ def run_training():
                 
                 #run the training
                 sess.run(train_step, feed_dict={x: train_images,\
-                                                y_: tf.one_hot(train_labels,num_output).eval()})#,\
-                                                #keep_prob:0.5})              
+                                                y_: tf.one_hot(train_labels,num_output).eval(),\
+                                                keep_prob:0.5})              
                     
     
                 step += 1
@@ -171,10 +245,10 @@ def run_training():
 
 inputDim = 48
 num_output_list = [100]
-num_epoch_list = [4000]
+num_epoch_list = [300]
 train_batch_size_list = [128]
 learning_rate_list = [1E-3]
-test_batch_size = 1000
+test_batch_size = 500
 
 #dataPath, LOGDIR, rawDataPath = fF.whichUser("Elliot")
 #savePath=LOGDIR
@@ -184,7 +258,7 @@ savePath = 'C:\\Users\\Sebastian\\Desktop\\MLChinese\\Saved_runs\\'
 localPath = 'C:\\Users\\Sebastian\\Desktop\\MLChinese\\CASIA\\1.0'
 
 
-name_of_run = '2conv_100Out_noPool'
+name_of_run = 'Generating_activations'
 
 for num_output in num_output_list:
     train_tfrecord_filename = \
